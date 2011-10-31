13601afcc10b650a3c369de3250b15176133acd4
Gertjan Assies <gertjan.assies@gmail.com>
2011-10-31 10:42:00 +0100

    app.js now starts new model

--- mobile/Resources/app.js
+++ mobile/Resources/app.js
@@ -14,84 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-Ti.include("windows/constants.js");
+Ti.include('/medicate.js');
 
-Titanium.API.info("starting application");
-
-var winHome = Titanium.UI.createWindow({
-    url:'windows/home.js',
-    title:'Home',
-    barColor: settings.color1,
-    color1: settings.color1,
-    color2: settings.color2
-});
-var tabHome = Titanium.UI.createTab({
-    icon:'images/home.png',
-    title:'Home',
-    window:winHome
-});
-var winStock = Titanium.UI.createWindow({
-    url:'windows/stock.js',
-    title:'Stock',
-    barColor: settings.color1,
-    color1: settings.color1,
-    color2: settings.color2
-});
-var tabStock = Titanium.UI.createTab({
-    icon:'images/stock.png',
-    title:'Stock',
-    window:winStock
-});
-var winDosages = Titanium.UI.createWindow({
-    url:'windows/dosages.js',
-    title:'Dosages',
-    barColor: settings.color1,
-    color1: settings.color1,
-    color2: settings.color2
-});
-var tabDosages = Titanium.UI.createTab({
-    icon:'images/dosages.png',
-    title:'Dosages',
-    window:winDosages
-});
-
-
-var winSettings = Titanium.UI.createWindow({
-    url:'windows/settings.js',
-    title:'Settings',
-    barColor: settings.color1,
-    color1: settings.color1,
-    color2: settings.color2
-});
-var tabSettings = Titanium.UI.createTab({
-    icon:'images/settings.png',
-    title:'Settings',
-    window:winSettings
-});
-
-var winMedication = Titanium.UI.createWindow({
-    url:'windows/medication.js',
-    title:'Medication',
-    barColor: settings.color1,
-    color1: settings.color1,
-    color2: settings.color2
-});
-var tabMedication = Titanium.UI.createTab({
-    icon:'images/medication.png',
-    title:'Medication',
-    window:winMedication
-});
-
-var tabGroup = Titanium.UI.createTabGroup();
-
-tabGroup.addTab(tabHome);
-tabGroup.addTab(tabMedication);
-tabGroup.addTab(tabStock);
-tabGroup.addTab(tabDosages);
-tabGroup.addTab(tabSettings);
-
-tabGroup.open({
-	transition:Titanium.UI.iPhone.AnimationStyle.CURL_UP
-});
+medicate.app.mainWindow = medicate.ui.createApplicationWindow();
+medicate.app.mainWindow.open();
 
 
